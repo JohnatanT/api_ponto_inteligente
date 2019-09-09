7b70d1d755516f55863960150e582832f2c6737e
Johnatan Tavares <johnatan.tavares@somacontadigital.com.br>
2019-09-09 09:59:11 -0300

    Adicionando service de funcionario

---
+++ src/main/java/com/johnatan/pontointeligente/api/service/impl/FuncionarioServiceImpl.java
@@ -0,0 +1,46 @@
+package com.johnatan.pontointeligente.api.service.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.johnatan.pontointeligente.api.entities.Funcionario;
+import com.johnatan.pontointeligente.api.repositories.FuncionarioRepository;
+import com.johnatan.pontointeligente.api.services.FuncionarioService;
+
+@Service
+public class FuncionarioServiceImpl implements FuncionarioService {
+
+	private static final Logger log = LoggerFactory.getLogger(FuncionarioServiceImpl.class);
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	@Override
+	public Funcionario persistir(Funcionario funcionario) {
+		log.info("Persistindo funcionário: {}", funcionario);
+		return this.funcionarioRepository.save(funcionario);
+	}
+
+	@Override
+	public Optional<Funcionario> buscaPorCpf(String cpf) {
+		log.info("Buscando funcionario pelo cpf: {}", cpf);
+		return Optional.ofNullable(this.funcionarioRepository.findByCpf(cpf));
+	}
+
+	@Override
+	public Optional<Funcionario> buscaPorEmail(String email) {
+		log.info("Buscando funcionario pelo email: {}", email);
+		return Optional.ofNullable(this.funcionarioRepository.findByEmail(email));
+	}
+
+	@Override
+	public Optional<Funcionario> buscaPorId(Long id) {
+		log.info("Buscando funcionario pelo Id: {}", id);
+		return this.funcionarioRepository.findById(id);
+	}
+	
+}
---
+++ src/main/java/com/johnatan/pontointeligente/api/services/FuncionarioService.java
@@ -0,0 +1,15 @@
+package com.johnatan.pontointeligente.api.services;
+
+import java.util.Optional;
+
+import com.johnatan.pontointeligente.api.entities.Funcionario;
+
+public interface FuncionarioService {
+	Funcionario persistir(Funcionario funcionario);
+	
+	Optional<Funcionario> buscaPorCpf(String cpf);
+	
+	Optional<Funcionario> buscaPorEmail(String email);
+	
+	Optional<Funcionario> buscaPorId(Long id);
+}
