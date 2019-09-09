2b9041e8f37a4af6b7923e2fcc6277dba393dba6
Johnatan Tavares <johnatan.tavares@somacontadigital.com.br>
2019-09-09 10:22:50 -0300

    dependencia e ajuste

--- src/main/java/com/johnatan/pontointeligente/api/repositories/LancamentoRepository.java
+++ src/main/java/com/johnatan/pontointeligente/api/repositories/LancamentoRepository.java
@@ -6,6 +6,7 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,6 +21,6 @@ public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 	
 	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
 	
-	//Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
+	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, PageRequest page);
 	
 }
